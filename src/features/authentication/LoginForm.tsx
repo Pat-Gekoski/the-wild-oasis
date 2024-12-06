c2410099b2a38135de9cba5498cbfdd33e65e9f2
Patric Gekoski <=>
2024-12-06 14:20:10 -0500

    Remove login credentials

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -1,14 +1,14 @@
-import { FormEvent, useState } from "react";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
-import { useLogin } from "./useLogin";
-import SpinnerMini from "../../ui/SpinnerMini";
+import { FormEvent, useState } from 'react'
+import Button from '../../ui/Button'
+import Form from '../../ui/Form'
+import Input from '../../ui/Input'
+import FormRowVertical from '../../ui/FormRowVertical'
+import { useLogin } from './useLogin'
+import SpinnerMini from '../../ui/SpinnerMini'
 
 function LoginForm() {
-	const [email, setEmail] = useState("PatricGekoski@pm.me");
-	const [password, setPassword] = useState("Valiscool$1");
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 	const { login, isLoading } = useLogin()
 
 	function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -16,42 +16,47 @@ function LoginForm() {
 		if (!email || !password) {
 			return
 		}
-		login({ email, password }, {
-			onSettled: () => {
-				setEmail('')
-				setPassword('')
-			}
-		})
+		login(
+			{ email, password },
+			{
+				onSettled: () => {
+					setEmail('')
+					setPassword('')
+				},
+			},
+		)
 	}
 
 	return (
-		<Form type="regular" onSubmit={handleSubmit}>
-			<FormRowVertical label="Email address">
+		<Form type='regular' onSubmit={handleSubmit}>
+			<FormRowVertical label='Email address'>
 				<Input
-					type="email"
-					id="email"
+					type='email'
+					id='email'
 					// This makes this form better for password managers
-					autoComplete="username"
+					autoComplete='username'
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 					disabled={isLoading}
 				/>
 			</FormRowVertical>
-			<FormRowVertical label="Password">
+			<FormRowVertical label='Password'>
 				<Input
-					type="password"
-					id="password"
-					autoComplete="current-password"
+					type='password'
+					id='password'
+					autoComplete='current-password'
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 					disabled={isLoading}
 				/>
 			</FormRowVertical>
 			<FormRowVertical>
-				<Button disabled={isLoading} $size="large">{!isLoading ? 'Login' : <SpinnerMini />}</Button>
+				<Button disabled={isLoading} $size='large'>
+					{!isLoading ? 'Login' : <SpinnerMini />}
+				</Button>
 			</FormRowVertical>
 		</Form>
-	);
+	)
 }
 
-export default LoginForm;
+export default LoginForm
